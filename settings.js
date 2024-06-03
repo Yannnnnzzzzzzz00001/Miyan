72c393f21b280aa30906dbc99c94af659da967d1
Yannnnnzzzzzzz00001 <mandryan5545@gmail.com>
2024-06-03 10:18:15 +0700

    Yan1784415105

--- settings.js
+++ settings.js
@@ -46,7 +46,7 @@ global.hituet = 0
 
 //bot sett
 global.typemenu = 'v12' // menu type 'v1' => 'v12'
-global.typereply = 'v4' // reply type 'v1' => 'v4'
+global.typereply = 'v2' // reply type 'v1' => 'v4'
 global.autoblocknumber = '92' //set autoblock country code
 global.antiforeignnumber = '91' //set anti foreign number country code
 global.welcome = false //welcome/left in groups
